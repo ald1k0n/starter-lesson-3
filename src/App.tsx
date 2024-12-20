0a0c928607ebae76cfba38317717391126a31f37
aseylkhanov <seylkhanov.aldiyar@gmail.com>
2024-12-20 20:48:18 +0500

    feat: jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { Input, Form, Button } from "antd";
-import useAuthorization from "./useAuthorization";
-
-export type User = {
-  email: string;
-  username: string;
-  password: string;
-};
+import { useAtom } from "jotai";
+import { countAtom } from "./atoms";
+import { Button, Typography } from "antd";
 
 function App() {
-  const [form] = Form.useForm();
-  const { isLoading, login } = useAuthorization();
+  const [count, setCount] = useAtom(countAtom);
+
+  const increment = () => setCount((c) => c + 1);
 
   return (
-    <Form form={form} onFinish={login} layout="vertical">
-      {/* <Form.Item name="username" label="username">
-        <Input />
-      </Form.Item> */}
-      <Form.Item name="email" label="email">
-        <Input />
-      </Form.Item>
-      <Form.Item name="password" label="password">
-        <Input />
-      </Form.Item>
-
-      <Button loading={isLoading} type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Typography.Title level={1}>{count}</Typography.Title>
+      <Button onClick={increment}>Increment</Button>
+    </>
   );
-
-  // return <>sdhfjdshf</>;
 }
 
 export default App;
